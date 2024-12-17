06cec70202772e47ebb78b81d7427d86bbf76bb8
fengjielin <fengjielin@DESKTOP-ENG5A6D>
2024-12-18 01:17:35 +0800

    函数，循环

---
+++ HelloWorldCS/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HelloWorldCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[0];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("请输入第" + (i + 1) + "位用户的信息：");
+                string info = GetUserInfo();
+
+                Array.Resize(ref array, array.Length + 1);
+                array[array.Length - 1] = info;
+            }
+
+            Console.WriteLine(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+
+        static string GetUserInfo()
+        {
+            Console.WriteLine("请输入你的姓名：");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("请输入你的故乡：");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("请输入你的爱好：");
+            string hobby = Console.ReadLine();
+
+            Console.WriteLine("请输入你的年龄：");
+            string age = Console.ReadLine();
+
+            name = changeData(name);
+
+            Console.WriteLine();
+            string newInfo = "你的名字是：" + name + "，故乡是：" + address + "，爱好是：" + hobby + "，年龄是" + age + "岁";
+            Console.WriteLine(newInfo);
+            Console.WriteLine();
+
+            return newInfo;
+        }
+
+        static string changeData( string name)
+        {
+            if (name == "张三")
+            {
+                name = "法外狂徒·张三";
+            }
+            return name;
+        }
+    }
+}
